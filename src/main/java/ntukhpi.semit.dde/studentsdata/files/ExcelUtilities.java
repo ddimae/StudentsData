91df2a190cda3b1ad2d011b7e72d2815c681df6f
milka_vlad <milkav06062003@gmail.com>
2023-05-14 22:51:26 +0300

    create method for creation excel F1

--- src/main/java/ntukhpi.semit.dde/studentsdata/files/ExcelUtilities.java
+++ src/main/java/ntukhpi.semit.dde/studentsdata/files/ExcelUtilities.java
@@ -4,10 +4,7 @@ import ntukhpi.semit.dde.studentsdata.entity.AcademicGroup;
 import ntukhpi.semit.dde.studentsdata.entity.Email;
 import ntukhpi.semit.dde.studentsdata.entity.PhoneNumber;
 import ntukhpi.semit.dde.studentsdata.entity.Student;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
@@ -18,6 +15,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -97,25 +95,26 @@ public class ExcelUtilities {
         return phones;
     }
 
-    public static String saveToWBExcel(String filename, List<Student> studentList) throws IOException {
+    public static String saveToWBExcel(String filename, Set<Student> studentList) throws IOException {
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet(filename);
+
+        // Стилі ячейки
+        CellStyle style = workbook.createCellStyle();
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
 
         int rowIndex = 0;
         for (Student student : studentList) {
             Row row = sheet.createRow(rowIndex++);
 
-            Cell firstNameCell = row.createCell(0);
-            ((Cell) firstNameCell).setCellValue(student.getFirstName());
+            Cell number = row.createCell(0);
+            number.setCellStyle(style);
+            ((Cell) number).setCellValue(rowIndex);
 
-            Cell lastNameCell = row.createCell(1);
-            lastNameCell.setCellValue(student.getLastName());
-
-            Cell middleNameCell = row.createCell(2);
-            middleNameCell.setCellValue(student.getMiddleName());
-
-            Cell dateOfBirthCell = row.createCell(3);
-            dateOfBirthCell.setCellValue(student.getDateOfBirth().toString());
+            addRowByForm1(row, student, style);
         }
 
         Path filePath = Paths.get("results/" + filename + ".xlsx");//
@@ -127,4 +126,20 @@ public class ExcelUtilities {
 
         return filePath.toAbsolutePath().toString();
     }
+
+    public static void addRowByForm1(Row row, Student student, CellStyle style) {
+
+        Cell lastNameCell = row.createCell(1);
+        lastNameCell.setCellStyle(style);
+        lastNameCell.setCellValue(student.getLastName().toUpperCase());
+
+        Cell firstNameCell = row.createCell(2);
+        firstNameCell.setCellStyle(style);
+        firstNameCell.setCellValue(student.getFirstName());
+
+        Cell middleNameCell = row.createCell(3);
+        middleNameCell.setCellStyle(style);
+        middleNameCell.setCellValue(student.getMiddleName());
+    }
+
 }
